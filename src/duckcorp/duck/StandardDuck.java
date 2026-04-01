8cb5fc68e5fac3a159c2a216a8569feb3804cad4
Arthur Gaussot <gaussota@3il.fr>
2026-04-01 09:33:46 +0200

    Feat : Sous-classe StandardDuck complétée

--- src/duckcorp/duck/StandardDuck.java
+++ src/duckcorp/duck/StandardDuck.java
@@ -3,13 +3,28 @@ package duckcorp.duck;
 /**
  * Canard en plastique standard.
  *
- * TODO (Ex1) :
- *   - Faites hériter cette classe de Duck
- *   - Implémentez le constructeur StandardDuck(int qualityScore) avec un appel à super
- *   - Implémentez getBasePrice() et describe()
  * @author Roussille Philippe <roussille@3il.fr>
  */
-public class StandardDuck {
+public class StandardDuck extends Duck {
 
     public static final double BASE_PRICE = 25.0;
+
+    /**
+     * Constructeur d'un canard standard.
+     * * @param qualityScore Le score de qualité (0 à 100)
+     */
+    public StandardDuck(int qualityScore) {
+        // Appel au constructeur de la classe parente Duck
+        super(DuckType.STANDARD, qualityScore);
+    }
+
+    @Override
+    public double getBasePrice() {
+        return BASE_PRICE;
+    }
+
+    @Override
+    public String describe() {
+        return "Canard Standard";
+    }
 }
