e8b6733935180a89ba0178f973b000bfad4f31ec
Arthur Gaussot <gaussota@3il.fr>
2026-04-01 09:36:26 +0200

    Feat : Sous-classe MiniDuck complétée

--- src/duckcorp/duck/MiniDuck.java
+++ src/duckcorp/duck/MiniDuck.java
@@ -3,13 +3,28 @@ package duckcorp.duck;
 /**
  * Canard en plastique miniature.
  *
- * TODO (Ex1) :
- *   - Faites hériter cette classe de Duck
- *   - Implémentez le constructeur MiniDuck(int qualityScore) avec un appel à super
- *   - Implémentez getBasePrice() et describe()
  * @author Roussille Philippe <roussille@3il.fr>
  */
-public class MiniDuck {
+public class MiniDuck extends Duck {
 
     public static final double BASE_PRICE = 12.0;
+
+    /**
+     * Constructeur d'un mini canard.
+     * @param qualityScore Le score de qualité (0 à 100)
+     */
+    public MiniDuck(int qualityScore) {
+        // Appel au constructeur de la classe parente Duck avec le type MINI
+        super(DuckType.MINI, qualityScore);
+    }
+
+    @Override
+    public double getBasePrice() {
+        return BASE_PRICE;
+    }
+
+    @Override
+    public String describe() {
+        return "Mini Canard";
+    }
 }
