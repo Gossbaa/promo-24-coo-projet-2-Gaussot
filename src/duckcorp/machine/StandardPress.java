9b98f6f9a9444934d1adea2e76b6043534a3ba89
Arthur Gaussot <gaussota@3il.fr>
2026-04-01 10:27:11 +0200

    Feat : Classe StandardPress complétée

--- src/duckcorp/machine/StandardPress.java
+++ src/duckcorp/machine/StandardPress.java
@@ -1,19 +1,41 @@
 package duckcorp.machine;
 
 import duckcorp.duck.Duck;
+import duckcorp.duck.DuckType;
+import duckcorp.duck.StandardDuck;
 
 /**
  * Presse produisant des canards Standard.
  *
- * TODO (Ex2) :
- *   - Faites hériter cette classe de Machine
- *   - Implémentez le constructeur sans paramètre avec un appel à super
- *   - Implémentez produceDuck(), getPurchaseCost(), getName()
  * @author Roussille Philippe <roussille@3il.fr>
  */
-public class StandardPress {
+public class StandardPress extends Machine {
 
     public static final int PURCHASE_COST    = 500;
     public static final int CAPACITY         = 5;
     public static final int MAINTENANCE_COST = 50;
+
+    /**
+     * Constructeur d'une presse standard.
+     */
+    public StandardPress() {
+        // Appel au constructeur de Machine avec le type de canard, la capacité et le coût d'entretien
+        super(DuckType.STANDARD, CAPACITY, MAINTENANCE_COST);
+    }
+
+    @Override
+    public Duck produceDuck() {
+        // Crée un nouveau StandardDuck en utilisant la qualité calculée par la machine
+        return new StandardDuck(computeQuality());
+    }
+
+    @Override
+    public int getPurchaseCost() {
+        return PURCHASE_COST;
+    }
+
+    @Override
+    public String getName() {
+        return "Presse Standard";
+    }
 }
