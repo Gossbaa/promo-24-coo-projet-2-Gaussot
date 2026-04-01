96f033061786fe8ee49b701cf4980c61172a1102
Arthur Gaussot <gaussota@3il.fr>
2026-04-01 09:39:04 +0200

    Feat : Sous-classe LuxuryDuck complétée

--- src/duckcorp/duck/LuxuryDuck.java
+++ src/duckcorp/duck/LuxuryDuck.java
@@ -3,13 +3,28 @@ package duckcorp.duck;
 /**
  * Canard en plastique de luxe.
  *
- * TODO (Ex1) :
- *   - Faites hériter cette classe de Duck
- *   - Implémentez le constructeur LuxuryDuck(int qualityScore) avec un appel à super
- *   - Implémentez getBasePrice() et describe()
  * @author Roussille Philippe <roussille@3il.fr>
  */
-public class LuxuryDuck {
+public class LuxuryDuck extends Duck {
 
     public static final double BASE_PRICE = 80.0;
+
+    /**
+     * Constructeur d'un canard de luxe.
+     * @param qualityScore Le score de qualité (0 à 100)
+     */
+    public LuxuryDuck(int qualityScore) {
+        // Appel au constructeur de la classe parente Duck avec le type LUXURY
+        super(DuckType.LUXURY, qualityScore);
+    }
+
+    @Override
+    public double getBasePrice() {
+        return BASE_PRICE;
+    }
+
+    @Override
+    public String describe() {
+        return "Canard de Luxe";
+    }
 }
