a9cdf1302660399b2653cba0f6a665a0a10b51ba
Arthur Gaussot <gaussota@3il.fr>
2026-04-01 10:29:35 +0200

    Feat : Classe MiniPress complétée

--- src/duckcorp/machine/MiniPress.java
+++ src/duckcorp/machine/MiniPress.java
@@ -1,19 +1,41 @@
 package duckcorp.machine;
 
 import duckcorp.duck.Duck;
+import duckcorp.duck.DuckType;
+import duckcorp.duck.MiniDuck;
 
 /**
  * Presse produisant des Mini Canards.
  *
- * TODO (Ex2) :
- *   - Faites hériter cette classe de Machine
- *   - Implémentez le constructeur sans paramètre avec un appel à super
- *   - Implémentez produceDuck(), getPurchaseCost(), getName()
  * @author Roussille Philippe <roussille@3il.fr>
  */
-public class MiniPress {
+public class MiniPress extends Machine {
 
     public static final int PURCHASE_COST    = 300;
     public static final int CAPACITY         = 8;
     public static final int MAINTENANCE_COST = 30;
+
+    /**
+     * Constructeur d'une mini-presse.
+     */
+    public MiniPress() {
+        // Appel au constructeur de Machine avec le type de canard, la capacité et le coût d'entretien
+        super(DuckType.MINI, CAPACITY, MAINTENANCE_COST);
+    }
+
+    @Override
+    public Duck produceDuck() {
+        // Crée un nouveau MiniDuck en utilisant la qualité calculée par la machine
+        return new MiniDuck(computeQuality());
+    }
+
+    @Override
+    public int getPurchaseCost() {
+        return PURCHASE_COST;
+    }
+
+    @Override
+    public String getName() {
+        return "Mini-Presse";
+    }
 }
